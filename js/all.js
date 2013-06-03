a4133a9017016291547e94f7c4524c6e2d4a2e90
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-06-03 08:29:42 +0200

    Add the GA tag.

--- js/all.js
+++ js/all.js
@@ -1,3 +1,3 @@
 // require_tree .
-// animate scrolling the page after clicking on a fragment link
-$(document).ready(function(){var e=$("html, body");$("a").click(function(){var t=$.attr(this,"href");return e.animate({scrollTop:$(t).offset().top},500,function(){window.location.hash=t}),!1})});
+// google analytics
+var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-40015611-1"]),_gaq.push(["_trackPageview"]),function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}(),$(document).ready(function(){var e=$("html, body");$("a").click(function(){var t=$.attr(this,"href");return e.animate({scrollTop:$(t).offset().top},500,function(){window.location.hash=t}),!1})});
