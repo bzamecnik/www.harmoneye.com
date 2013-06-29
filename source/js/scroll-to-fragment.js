3491f048d1426d5afff83ca928b37417cae79aa9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-06-30 01:31:29 +0200

    Refactor the common javascript into files. Use CDN for Modernizr.

---
+++ source/js/scroll-to-fragment.js
@@ -0,0 +1,13 @@
+// animate scrolling the page after clicking on a fragment link
+$(document).ready(function() {
+  var $root = $('html, body');
+  $('a').click(function() {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+  });
+});
---
+++ source/js/sharethis.js
@@ -0,0 +1,5 @@
+// ===== Share This =====
+
+// stLight.options({publisher: "70fe0e1f-a7c3-4a6f-97b1-7aec5e22c1a2", doNotHash: false, doNotCopy: false, hashAddressBar: false});
+// var options={ "publisher": "70fe0e1f-a7c3-4a6f-97b1-7aec5e22c1a2", "position": "left", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "googleplus", "twitter", "email"]}};
+// var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
