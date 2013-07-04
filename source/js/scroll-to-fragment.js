4e5445f3aae4709686181fe2f1f291f0de39662b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-07-04 08:42:00 +0200

    Fix the "scroll to fragment" JS.

--- source/js/scroll-to-fragment.js
+++ source/js/scroll-to-fragment.js
@@ -3,11 +3,14 @@ $(document).ready(function() {
   var $root = $('html, body');
   $('a').click(function() {
       var href = $.attr(this, 'href');
-      $root.animate({
-          scrollTop: $(href).offset().top
-      }, 500, function () {
-          window.location.hash = href;
-      });
-      return false;
+			if (href && href.match(/^#[^#]+/)) {
+	      $root.animate({
+	          scrollTop: $(href).offset().top
+	      }, 500, function () {
+	          window.location.hash = href;
+	      });
+				return false;
+			}
+			return true;
   });
 });
